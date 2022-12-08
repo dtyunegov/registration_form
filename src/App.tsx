f00c7d93776845ab87a4e8140b678bd3ae965ccf
dtyunegov <dtyunegov@gmail.com>
2022-12-08 15:42:38 +0300

    UI-1: add validation

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import './App.css'
 import background from './assets/images/background.png'
-import React, { useEffect, useState } from "react"
+import { useState } from "react"
 
 function App() {
   const [values, setInputValue] = useState({
@@ -19,19 +19,22 @@ function App() {
 
   function handleChange(event: any) {
     const { name, value } = event.target
-    console.log(name)
     setInputValue({ ...values, [name]: value })
     setValidation({ ...validation, [name]: '' })
     switch (name) {
       case "login":
-        if (!value.match(/^((\w|\s)+)$/g)) setValidation({ ...validation, login: 'Login is incorrect' })
+        if (!value.match(/^((\w|\s)+)$/g)) setValidation({ ...validation, [name]: 'Login is incorrect' })
         break
       case "email":
-        if (!value.match(/\S{2,}@\S{1,}(\.\S{1,}){1,}/g) || [...value].filter(s => s == '@').length > 1 || value.indexOf('..') > -1) setValidation({ ...validation, email: 'Email is incorrect' })
+        if (!value.match(/\S{2,}@\S{1,}(\.\S{1,}){1,}/g) || [...value].filter(s => s == '@').length > 1 || value.indexOf('..') > -1) setValidation({ ...validation, [name]: 'Email is incorrect' })
+        break
+      case "password":
+        if (!value.match(/(?=.*[0-9])(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z]).{6,}/g)) setValidation({ ...validation, [name]: 'Password is incorrect' })
+        break
+      case "confirmPassword":
+        if (values.password != value) setValidation({ ...validation, [name]: 'Passwords do not match' })
         break
     }
-
-    // setValidation({ ...validation, [name]: "" })
   }
 
   const valueTrim = (event: any) => {
@@ -39,32 +42,15 @@ function App() {
     setInputValue({ ...values, [name]: value.trim() })
   }
 
-  // const setLogin = (e: any) => {
-  //   const value = e.target.value
-  //   validation.login = ''
-  //   setInputValue({ ...values, login: value })
-  //   if (!value.match(/^((\w|\s)+)$/g)) setValidation({ ...validation, login: 'Login is incorrect' })
-  // }
-
-  const checkEmail = (e: any) => {
-    setInputValue({ ...values, email: values.email.trim() })
-    if (!values.email.trim()) setValidation({ ...validation, email: 'Email is required' })
-  }
-
-  const checkPassword = (e: any) => {
-    if (!values.password.trim()) setValidation({ ...validation, password: 'Password is required' })
-  }
-
-  const checkConfirmPassword = (e: any) => {
-    if (!values.confirmPassword.trim()) setValidation({ ...validation, confirmPassword: 'Confirm password is required' })
-  }
-
-  const handleSubmit = (e: any) => {
-    e.preventDefault()
+  const handleSubmit = (event: any) => {
     if (!values.login.trim()) validation.login = 'Login is required'
-    if (!values.login.trim()) validation.email = 'Email is required'
+    if (!values.email.trim()) validation.email = 'Email is required'
     if (!values.password.trim()) validation.password = 'Password is required'
     if (!values.confirmPassword.trim()) validation.confirmPassword = 'Confirm password is required'
+
+    const preventDefault = Object.entries(validation).some(e => e[1])
+    if (preventDefault) event.preventDefault()
+    else alert("Success")
 
     setValidation({ ...validation })
   }
@@ -90,7 +76,7 @@ function App() {
           placeholder='Email'
           name='email'
           onChange={(e) => handleChange(e)}
-          onBlur={(e) => checkEmail(e)}
+          onBlur={(e) => valueTrim(e)}
           value={values.email}
         />
         {validation.email && <p className='text-red-600'>{validation.email}</p>}
@@ -100,7 +86,6 @@ function App() {
           placeholder='Password'
           name='password'
           onChange={(e) => handleChange(e)}
-          onBlur={(e) => checkPassword(e)}
           value={values.password}
         />
         {validation.password && <p className='text-red-600'>{validation.password}</p>}
@@ -110,7 +95,6 @@ function App() {
           placeholder='Confirm password'
           name='confirmPassword'
           onChange={(e) => handleChange(e)}
-          onBlur={(e) => checkConfirmPassword(e)}
           value={values.confirmPassword}
         />
         {validation.confirmPassword && <p className='text-red-600'>{validation.confirmPassword}</p>}
